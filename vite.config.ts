0f3c3c7341c282a87b8f0508be8ba44d518dc64b
Jay Zuo <yjzuo6@gmail.com>
2025-03-10 18:10:22 -0400

    lets try docs

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,8 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), svgr()],
+  build: {
+    outDir: 'docs',
+    emptyOutDir: true, // also necessary
+  },
 });
