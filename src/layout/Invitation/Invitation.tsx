bc512075e1169aaa0e285c43643e509de4e54dc5
Jay Zuo <yjzuo6@gmail.com>
2025-03-10 20:11:00 -0400

    change gcal event

--- src/layout/Invitation/Invitation.tsx
+++ src/layout/Invitation/Invitation.tsx
@@ -10,15 +10,10 @@ const Invitation = () => {
       The peak of Vancouver
       <RoundButton
         target="_blank"
-        href="https://calendar.google.com/calendar/event?action=TEMPLATE&tmeid=NGNybzY0ZzNiZjB0dWg0NHExZHIxYTBldTAgZmFtaWx5MTA3NjA1OTEwNzQ0OTM1MjE2ODhAZw&tmsrc=family10760591074493521688%40group.calendar.google.com"
+        href="https://www.addevent.com/event/Tc25153249+google"
         rel="noreferrer">
         Click here to add to your calendar!
       </RoundButton>
-      <div
-        className="content"
-        dangerouslySetInnerHTML={{
-          __html: ` <a target="_blank" href="https://calendar.google.com/calendar/event?action=TEMPLATE&amp;tmeid=NGNybzY0ZzNiZjB0dWg0NHExZHIxYTBldTAgZmFtaWx5MTA3NjA1OTEwNzQ0OTM1MjE2ODhAZw&amp;tmsrc=family10760591074493521688%40group.calendar.google.com"><img border="0" src="https://calendar.google.com/calendar/images/ext/gc_button1_en.gif" alt="Google Calendar"></a>`,
-        }}></div>
     </InvitationWrapper>
   );
 };
